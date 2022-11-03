b8e188a97497e05761979bd03fcfa706ba3649a6
yaalyy <yaalyy@outlook.com>
2022-11-03 23:44:43 +0000

    new files

---
+++ c_coursework.c
@@ -0,0 +1,11 @@
+#include "graphics.h"
+#include<stdio.h>
+
+const int width = 50;    //Set the window size
+const int height = 50;
+
+int main(int argc, char **argv)
+{
+	printf("hello world\n");
+	return 0;
+}
